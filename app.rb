775012b3cf2f3adbd4365f2de6d3cfbb80366b40
matcharr <you@example.com>
2020-07-20 20:48:40 +0200

    done_part_01

--- app.rb
+++ app.rb
@@ -1,1 +1,22 @@
 # frozen_string_literal: true
+
+require 'bundler'
+Bundler.require
+
+require_relative 'lib/game'
+require_relative 'lib/player'
+
+
+john = Player.new("john")
+alice = Player.new("alice")
+
+while !((alice.life_points || john.life_points) <= 0) 
+    puts "\nVoici l'état de chaque joueur : "
+    alice.show_state
+    puts "\nPassons à la phase d'attaque : "
+    alice.attacks(john)
+    break if (john.life_points <= 0)
+    john.attacks(alice)
+end
+
+binding.pry
--- lib/player.rb
+++ lib/player.rb
@@ -1,1 +1,28 @@
 # frozen_string_literal: true
+
+class Player
+    attr_accessor :name, :life_points
+    def initialize(name)
+        @name = name
+        @life_points = 10
+    end
+
+    def show_state
+        puts "#{@name} have #{@life_points} life points"
+    end
+
+    def gets_damage(x)
+        @life_points -= x
+        if @life_points >= 0 ? show_state : (@life_points = 0 ; puts "le joueur #{name} a été tué !")
+        end
+    end
+
+    def attacks(player)
+        damage = compute_damage
+        player.gets_damage(damage)
+    end
+    
+    def compute_damage
+        return rand(1..6)
+    end
+end
