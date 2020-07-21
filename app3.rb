6a6dbda8135defbd05e9b653eb66313fbcf6a0bc
matcharr <you@example.com>
2020-07-21 17:58:36 +0200

    issue with first bot damage

--- app3.rb
+++ app3.rb
@@ -7,10 +7,24 @@ require_relative 'lib/game'
 require_relative 'lib/player'
 
 puts " -----------------------------------------------\n|Bienvenue sur 'ILS VEULENT TOUS MA POO' !      |\n|Le but du jeu est d'être le dernier survivant !|\n -----------------------------------------------\n"
-
-my_game = Game.new("Wolverine")
+puts "\n----------Initialize player----------\n\n"
+print ' > Enter player name : '
+my_game = Game.new(gets.chomp)
 my_game.show_players
-#my_game.kill_player()
-my_game.menu
-my_game.menu_choice(gets.chomp)
+puts "\n------------Let's begin the fight!-----------"
+while my_game.is_still_ongoing?
+  puts "\n----------Menu-----------------------"
+  my_game.menu
+  puts "\n----------Your Turn------------------"
+  print '> '
+  my_game.menu_choice(gets.chomp)
+  #sleep 2
+  puts "\n----------Enemies Turn---------------"
+  my_game.enemies_attack
+  #sleep 2
+  puts "\n----------Players Stats--------------"
+  my_game.show_players
+  #sleep 2
+end
+my_game.end
 binding.pry
--- lib/game.rb
+++ lib/game.rb
@@ -17,8 +17,8 @@ class Game
 
   def show_players
     @human_player.show_state
-    puts "left #{@enemies.length} enemies!"
-    [0, 1, 2, 3].each { |n| @enemies[n].show_state }
+    #@enemies.each { |i| @enemies[i].show_state }
+    puts "#{@enemies.length} left enemies!"
   end
 
   def menu
@@ -26,26 +26,29 @@ class Game
     puts 'a - Search Weapon'
     puts "s - Search Health Pack\n"
     puts "\nAttacking first enemies in sight!!!\n\n"
-    @enemies.each {|i| puts "#{@enemies.index(i) + 1} - #{i.name} have #{i.life_points} life points." if i.life_points > 0}
-    puts 'quit - quit game'
+    @enemies.each do |i|
+      puts "#{@enemies.index(i)} - #{i.name} have #{i.life_points} life points." if i.life_points > 0
+    end
   end
 
   def menu_choice(string)
-    if string == "a" || string == "s"
-        case string
-        when "a" then @human_player.search_weapon
-        when "s" then @human_player.search_health_pack
-        end
+    if string == 'a' || string == 's'
+      case string
+      when 'a' then @human_player.search_weapon
+      when 's' then @human_player.search_health_pack
+      end
     else
-        @human_player.attacks(@enemies[string.to_i]) if choice.to_i != 0 && @ennemies[choice.to_i - 1]
+      @human_player.attacks(@enemies[string.to_i]) if string.to_i != 0 && @enemies[string.to_i - 1]
     end
-    @enemies.each {|index| kill_player(index) if index.life_points <= 0}
+    @enemies.each { |index| kill_player(index) if index.life_points <= 0 }
   end
-  def   enemies_attack
+
+  def enemies_attack
     @enemies.each { |index| index.attacks(@human_player) if index.life_points > 0 }
   end
+
   def end
-    puts "La partie est terminée"
-    puts @human_player.life_points > 0 ? "Well done ! You win !" : "ARRR ! You loose"
+    puts 'La partie est terminée'
+    puts @human_player.life_points > 0 ? 'Well done ! You win !' : 'ARRR ! You loose'
   end
 end
